d8394dbe561fecb610e34e9c5c60cfaff7a619ae
batchmode <batchmode@gmail.com>
2022-10-16 17:29:03 +0200

    add github icon

--- src/client/Version.jsx
+++ src/client/Version.jsx
@@ -1,12 +1,17 @@
-const Version = () => {
-    const version = import.meta.env.VITE_COMMIT_SHA
+import {FaGithub} from "react-icons/all";
 
-    if (version && version.length > 6) {
-        return (<a className="text-xs text-blue-300"
-                   href={"https://github.com/batchmode/dsp-production-overview/tree/" + version} target="_blank">{version.substring(0, 7)}</a>)
-    } else {
-        return <div className="text-xs text-blue-300">undefined</div>
-    }
+
+const Version = () => {
+    const version = import.meta.env.VITE_COMMIT_SHA || "develop"
+
+    return (<a className="text-blue-400"
+               href={"https://github.com/batchmode/dsp-production-overview/tree/" + version}
+               target="_blank">
+        <div className="flex gap-1 items-center">
+            <FaGithub/>
+            <div className="text-xs">{version.substring(0, 7)}</div>
+        </div>
+    </a>)
 
 }
 
