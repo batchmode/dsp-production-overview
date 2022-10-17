18662789928beaf44c79890800d92543ecc5d91e
batchmode <batchmode@gmail.com>
2022-10-17 08:46:01 +0200

    let develop version link to repository

--- src/client/Version.jsx
+++ src/client/Version.jsx
@@ -4,9 +4,11 @@ import {FaGithub} from "react-icons/all";
 const Version = () => {
     const version = import.meta.env.VITE_COMMIT_SHA || "develop"
 
-    return (<a className="text-blue-400"
-               href={"https://github.com/batchmode/dsp-production-overview/tree/" + version}
-               target="_blank">
+    const link =  (version !== "develop")
+        ? "https://github.com/batchmode/dsp-production-overview/tree/" + version
+        : "https://github.com/batchmode/dsp-production-overview"
+
+    return (<a className="text-blue-400" href={link} target="_blank">
         <div className="flex gap-1 items-center">
             <FaGithub/>
             <div className="text-xs">{version.substring(0, 7)}</div>
