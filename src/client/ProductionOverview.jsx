8a82ba21c09c523dcc6074dad2709a7502e50057
batchmode <batchmode@gmail.com>
2022-10-17 20:29:22 +0200

    set initial filter

--- src/client/ProductionOverview.jsx
+++ src/client/ProductionOverview.jsx
@@ -36,7 +36,7 @@ const ProductionOverview = ({system, planet, model, updateModel, onClose}) => {
     ) : (<></>)
 
     const warningIcon = productionChains.find(chain => chain.hasMissingRecipes)
-        ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="darkorange" className="w-6 h-6" onClick={_ => setShowConfigureRecipes({show: true})}>
+        ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="darkorange" className="w-6 h-6" onClick={_ => setShowConfigureRecipes({show: true, filter: ""})}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
             </svg>
         )
@@ -56,7 +56,7 @@ const ProductionOverview = ({system, planet, model, updateModel, onClose}) => {
                 </div>
                 <button
                     className="capitalize text-xs text-blue-400 border border-1 border-gray-400 rounded p-1 bg-white hover:bg-gray-200"
-                    onClick={_ => setShowConfigureRecipes({show: true})}>
+                    onClick={_ => setShowConfigureRecipes({show: true, filter: ""})}>
                     Configure Recipes
                 </button>
             </div>
