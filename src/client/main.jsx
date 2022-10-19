83cfb74e3a9edff4d43fec571c32c59195b6bf90
batchmode <batchmode@gmail.com>
2022-10-19 22:46:42 +0200

    lift HandleError on level up

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import HandleError from "./HandleError.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <HandleError>
+            <App/>
+        </HandleError>
+    </React.StrictMode>
 )
