a46a3cd35a0bf2b6c43f62923bdcb3c24de2534f
batchmode <batchmode@gmail.com>
2022-10-19 22:12:40 +0200

    add production rate dialog to ProductionOverview

--- src/client/ProductionOverview.jsx
+++ src/client/ProductionOverview.jsx
@@ -3,16 +3,25 @@ import {useState} from "react";
 import Popup from "./Popup.jsx";
 import ConfigureRecipes from "./ConfigureRecipes.jsx";
 import useProductionChains from "./useProductionChains.jsx";
+import ProductionRate from "./ProductionRate.jsx";
 
 const ProductionOverview = ({system, planet, model, updateModel, onClose}) => {
 
     const [showConfigureRecipes, setShowConfigureRecipes] = useState({show: false})
+    const [showSetProductionRate, setShowSetProductionRate] = useState({show: false})
     const productionChains = useProductionChains(planet, model)
 
     if (!planet) return (<></>)
 
-    const handleClickProduct = (product) => {
-        setShowConfigureRecipes({show: true, filter: product.name})
+    const handleClickProduct = (product, action) => {
+        switch (action) {
+            case "showRecipes":
+                setShowConfigureRecipes({show: true, filter: product.name})
+                break
+            case "setProductionRate":
+                setShowSetProductionRate({show: true, product})
+                break
+        }
     }
 
     const handleClickClose = _ => {
@@ -27,17 +36,29 @@ const ProductionOverview = ({system, planet, model, updateModel, onClose}) => {
         )
     })
 
-    const popup = showConfigureRecipes.show ? (
-        <Popup>
-            <ConfigureRecipes system={system} planet={planet} model={model} updateModel={updateModel}
-                              onClose={_ => setShowConfigureRecipes({show: false})}
-                              initialFilter={showConfigureRecipes.filter}/>
-        </Popup>
-    ) : (<></>)
+    const recipesPopup = showConfigureRecipes.show
+        ? (
+            <Popup>
+                <ConfigureRecipes system={system} planet={planet} model={model} updateModel={updateModel}
+                                  onClose={_ => setShowConfigureRecipes({show: false})}
+                                  initialFilter={showConfigureRecipes.filter}/>
+            </Popup>
+        )
+        : (<></>)
+
+    const productionRatePopup = showSetProductionRate.show
+        ? (
+            <Popup>
+                <ProductionRate planet={planet} product={showSetProductionRate.product} onClose={_ => setShowSetProductionRate({show: false})}/>
+            </Popup>
+        )
+        : (<></>)
 
     const warningIcon = productionChains.find(chain => chain.hasMissingRecipes)
-        ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="darkorange" className="w-6 h-6" onClick={_ => setShowConfigureRecipes({show: true, filter: ""})}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
+        ? (<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="darkorange" className="w-6 h-6"
+                onClick={_ => setShowConfigureRecipes({show: true, filter: ""})}>
+                <path strokeLinecap="round" strokeLinejoin="round"
+                      d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"/>
             </svg>
         )
         : (<></>)
@@ -65,7 +86,8 @@ const ProductionOverview = ({system, planet, model, updateModel, onClose}) => {
                     {productionItems}
                 </div>
             </div>
-            {popup}
+            {recipesPopup}
+            {productionRatePopup}
         </div>
     )
 }
