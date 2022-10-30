80f938843a6a87dde651289e541ad422a98a083d
batchmode <batchmode@gmail.com>
2022-10-30 08:53:01 +0100

    read base url from .env config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '/dsp-production-overview/'
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), 'VITE')
+
+    return {
+        plugins: [react()],
+        base: env.VITE_BASE
+    }
 })
